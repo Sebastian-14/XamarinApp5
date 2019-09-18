f610b2ec974831f2b81b6f665f51a2eb8df37b45
Sebastian <sebas14121999@gmail.com>
2019-09-17 22:03:56 -0500

    Agregar archivos de proyecto.

---
+++ App5/App5/button.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace App5
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class button : ContentPage
+    {
+        public button()
+        {
+            InitializeComponent();
+        }
+        async void OnButtonClicked(object sender, EventArgs args)
+        {
+
+            await label.RelRotateTo(360, 1000);
+        }
+
+    }
+}
